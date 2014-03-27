c43bde136cd1526eecb855ea94579315dfd0d47a
Kenneth Lee <kenneth.lee@cbc.ca>
2014-03-27 13:57:27 -0400

    fixed deck bugs in drawAt and logic for turns

--- test/deck.js
+++ test/deck.js
@@ -34,16 +34,23 @@ describe('Deck', function() {
     it('should deal cards from the deck', function(done) {
         var deck = new Deck([1, 2, 3, 4, 5]),
             targetDeck1 = new Deck(),
-            targetDeck2 = new Deck();
+            targetDeck2 = new Deck([8]); // Start with another card
 
         // deal out 4 of the 5 cards
         deck.deal(2, [targetDeck1, targetDeck2]);
 
         deck.cards.length.should.equal(1); // check remaining
 
-        // ensure cards are dealt out equally
+        // ensure number of cards are dealt out
         targetDeck1.cards.length.should.equal(2);
-        targetDeck2.cards.length.should.equal(2);
+        targetDeck2.cards.length.should.equal(3);
+
+        // ensure cards are in proper places
+        targetDeck1.cards[0].should.equal(1);
+        targetDeck1.cards[1].should.equal(2);
+        targetDeck2.cards[0].should.equal(3);
+        targetDeck2.cards[1].should.equal(4);
+        targetDeck2.cards[2].should.equal(8);
 
         done();
     });
@@ -100,7 +107,23 @@ describe('Deck', function() {
         done();
     });
 
-    it('should draw cards from anywhere in deck', function(done) {
+    it('should draw a card from anywhere in deck', function(done) {
+        var deck = new Deck([2, 4, 6, 4]);
+
+        var cards = deck.drawAt(1);
+
+        deck.cards.length.should.equal(3);
+        deck.cards[0].should.equal(2);
+        deck.cards[1].should.equal(6);
+        deck.cards[2].should.equal(4);
+
+        cards.length.should.equal(1);
+        cards[0].should.equal(4);
+
+        done();
+    });
+
+    it('should draw many cards from anywhere in deck', function(done) {
         var deck = new Deck([1, 2, 3, 4, 5]);
 
         var cards = deck.drawAt(0, 4, 2);
@@ -110,9 +133,9 @@ describe('Deck', function() {
         deck.cards[1].should.equal(4);
 
         cards.length.should.equal(3);
-        cards[0].should.equal(1);
-        cards[1].should.equal(5);
-        cards[2].should.equal(3);
+        cards.filter(function(card) {
+            return ~[1, 3, 5].indexOf(card);
+        }).length.should.equal(3);
 
         done();
     });
--- test/game.js
+++ test/game.js
@@ -13,14 +13,77 @@ describe('Game', function() {
             game.on('init', function(err) {
                 assert.ifError(err);
 
-                game.deck.cards.length.should.equal(25);
                 game.players.length.should.equal(2);
 
                 done();
             });
         });
 
-        it('should allow one team to win');
+        it('should always have 5 cards in hand when only moving', function(done) {
+            var game = new Game('1v1');
+
+            game.on('init', function(err) {
+
+                var playTurn = function(action, game) {
+                    action.player.hand.cards.length.should.equal(5);
+
+                    action.moveBackwards(0) || action.move(0);
+                };
+
+                game.players[0].on('turn', playTurn);
+                game.players[1].on('turn', playTurn);
+
+                game.on('end', function(err) {
+                    assert.ifError(err);
+
+                    game.turnCount.should.equal(15);
+
+                    done();
+                });
+
+                game.start();
+            });
+        });
+
+        it.skip('should allow one team to win', function(done) {
+            var game = new Game('1v1');
+
+            game.on('init', function(err) {
+
+                var playTurn = function(action, game) {
+                    action.moveBackwards(0) || action.move(0);
+                };
+
+                game.players[0].on('turn', playTurn);
+                game.players[1].on('turn', playTurn);
+
+                // Only one should be triggered
+                game.players[0].on('win', done);
+                game.players[1].on('win', done);
+
+                game.start();
+            });
+        });
+
+        it.skip('should allow one team to lose', function(done) {
+            var game = new Game('1v1');
+
+            game.on('init', function(err) {
+
+                var playTurn = function(action, game) {
+                    action.moveBackwards(0) || action.move(0);
+                };
+
+                game.players[0].on('turn', playTurn);
+                game.players[1].on('turn', playTurn);
+
+                // Only one should be triggered
+                game.players[0].on('lose', done);
+                game.players[1].on('lose', done);
+
+                game.start();
+            });
+        });
     });
 
     describe('2v2', function() {
@@ -30,8 +93,10 @@ describe('Game', function() {
             game.on('init', function(err) {
                 assert.ifError(err);
 
-                game.deck.cards.length.should.equal(50);
                 game.players.length.should.equal(4);
+                game.players.forEach(function(player) {
+                    player.hand.cards.length.should.equal(5);
+                });
 
                 done();
             });
@@ -63,6 +128,10 @@ describe('Game', function() {
 
                     action.should.be.an.instanceOf(Game.Action);
                     game.should.be.an.instanceOf(Game);
+
+                    game.players.forEach(function(player) {
+                        player.hand.cards.length.should.equal(5);
+                    });
 
                     done();
                 });
