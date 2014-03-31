1fb1b258fb9e10b30d3fdd1f0bd8301872259a02
Kenneth Lee <kenneth.lee@cbc.ca>
2014-03-31 17:15:13 -0400

    something is very wrong

--- test/game.js
+++ test/game.js
@@ -4,6 +4,23 @@ var assert = require('assert'),
     Player = flashDuel.Player,
     Game = flashDuel.Game;
 
+// Some helper functions
+var moveForward = function(action) {
+    return action.move(0)
+        || action.move(1)
+        || action.move(2)
+        || action.move(3)
+        || action.move(4);
+};
+
+var moveBackwards = function(action) {
+    return action.moveBackwards(0)
+        || action.moveBackwards(1)
+        || action.moveBackwards(2)
+        || action.moveBackwards(3)
+        || action.moveBackwards(4);
+};
+
 describe('Game', function() {
 
     describe('1v1', function() {
@@ -27,7 +44,7 @@ describe('Game', function() {
                 var playTurn = function(action, game) {
                     action.player.hand.cards.length.should.equal(5);
 
-                    action.moveBackwards(0) || action.move(0);
+                    moveBackwards(action) || moveForward(action);
                 };
 
                 game.players[0].on('turn', playTurn);
@@ -38,6 +55,43 @@ describe('Game', function() {
 
                     game.turnCount.should.equal(15);
 
+                    done();
+                });
+
+                game.start();
+            });
+        });
+
+        it('should win and lose', function(done) {
+            var game = new Game('1v1');
+
+            game.on('init', function(err) {
+                var wins = 0,
+                    loses = 0;
+
+                var playTurn = function(action, game) {
+                    moveBackwards(action) || moveForward(action);
+                };
+
+                var countWin = function() {
+                    wins++;
+                };
+
+                var countLose = function() {
+                    loses++;
+                };
+
+                game.players[0].on('turn', playTurn);
+                game.players[1].on('turn', playTurn);
+
+                for (var i = 0; i < 2; i++) {
+                    game.players[i].on('win', countWin);
+                    game.players[i].on('lose', countLose);
+                }
+
+                game.on('end', function() {
+                    wins.should.equal(1);
+                    loses.should.equal(1);
                     done();
                 });
 
@@ -59,6 +113,45 @@ describe('Game', function() {
                 });
 
                 done();
+            });
+        });
+
+        it('should win and lose', function(done) {
+            var game = new Game('2v2');
+
+            game.on('init', function(err) {
+                var wins = 0,
+                    loses = 0;
+
+                var playTurn = function(action, game) {
+                    moveBackwards(action) || moveForward(action);
+                };
+
+                var countWin = function() {
+                    wins++;
+                };
+
+                var countLose = function() {
+                    loses++;
+                };
+
+                game.players[0].on('turn', playTurn);
+                game.players[1].on('turn', playTurn);
+                game.players[2].on('turn', playTurn);
+                game.players[3].on('turn', playTurn);
+
+                for (var i = 0; i < 4; i++) {
+                    game.players[i].on('win', countWin);
+                    game.players[i].on('lose', countLose);
+                }
+
+                game.on('end', function() {
+                    wins.should.equal(2);
+                    loses.should.equal(2);
+                    done();
+                });
+
+                game.start();
             });
         });
     });
@@ -138,54 +231,6 @@ describe('Game', function() {
             });
         });
 
-        it('should win', function(done) {
-            var game = new Game('1v1');
-
-            game.on('init', function(err) {
-
-                var playTurn = function(action, game) {
-                    action.moveBackwards(0) || action.move(0);
-                };
-
-                var verify = function() {
-                    done();
-                };
-
-                game.players[0].on('turn', playTurn);
-                game.players[1].on('turn', playTurn);
-
-                // Only one should be triggered
-                game.players[0].on('win', verify);
-                game.players[1].on('win', verify);
-
-                game.start();
-            });
-        });
-
-        it('should lose', function(done) {
-            var game = new Game('1v1');
-
-            game.on('init', function(err) {
-
-                var playTurn = function(action, game) {
-                    action.moveBackwards(0) || action.move(0);
-                };
-
-                var verify = function() {
-                    done();
-                };
-
-                game.players[0].on('turn', playTurn);
-                game.players[1].on('turn', playTurn);
-
-                // Only one should be triggered
-                game.players[0].on('lose', verify);
-                game.players[1].on('lose', verify);
-
-                game.start();
-            });
-        });
-
         it('should be able to attack and defend or die', function(done) {
             var game = new Game('1v1');
 
@@ -197,25 +242,21 @@ describe('Game', function() {
                         attack = action.player.hand.cards.indexOf(distance);
 
                     if (~attack) {
-                        action.attack([attack], two);    // Attack if possible
+                        action.attack(attack, two);    // Attack if possible
                     } else {
-                        // Forward
-                        action.move(0) || action.moveBackwards(0)
-                        || action.move(1) || action.moveBackwards(1);
+                        moveForward(action) || moveBackwards(action); // Forward!
                     }
                 };
 
                 var playTurnTwo = function(action, game) {
-                    // Forward! copy pasta should really use for loop or something
-                    var result = action.move(0) || action.moveBackwards(0)
-                                || action.move(1) || action.moveBackwards(1)
-                                || action.move(2) || action.moveBackwards(2)
-                                || action.move(3) || action.moveBackwards(3)
-                                || action.move(4) || action.moveBackwards(4);
-
-                    if (!result) {
-                        console.log(action.player.hand);
+                    console.log(action.player.peice);
+                    console.log(action.player.hand);
+                    // Too close to other side
+                    if (action.player.peice.position <= 10) {
+                        return moveBackwards(action);
                     }
+
+                    moveForward(action) || moveBackwards(action); // Forward!
                 };
 
                 var defendTwo = function(defend, game) {
@@ -239,6 +280,66 @@ describe('Game', function() {
 
                 // Only one should be triggered
                 game.players[0].on('win', verify);
+
+                game.start();
+            });
+        });
+
+        it('should be able to dashing strike and retreat/die', function(done) {
+            var game = new Game('1v1');
+
+            game.on('init', function(err) {
+
+                var playTurnOne = function(action, game) {
+                    var two = action.getPlayer('two'),
+                        distance = two.peice.position - action.player.peice.position;
+
+                    for (var moveIndex = 0; moveIndex < 5; moveIndex++) {
+                        for (var attackIndex = moveIndex; attackIndex < 5; attackIndex++) {
+                            if (moveIndex !== attackIndex && action.player.hand.peekAt(moveIndex)[0] + action.player.hand.peekAt(attackIndex)[0] === distance) {
+                                return action.dashingStrike(moveIndex, attackIndex, two);    // Attack if possible
+                            }
+                        }
+                    }
+
+                    if (distance <= 10) {
+                        return moveBackwards(action);
+                    }
+
+                    moveForward(action) || moveBackwards(action); // Forward!
+                };
+
+                var playTurnTwo = function(action, game) {
+                    var one = action.getPlayer('one'),
+                        distance = action.player.peice.position - one.peice.position;
+
+                    //console.log(action.player.peice);
+                    //console.log(action.player.hand);
+
+                    if (distance <= 5 || action.player.peice.position <= 11) {
+                        return moveBackwards(action);
+                    }
+
+                    moveForward(action) || moveBackwards(action); // Forward!
+                };
+
+                var defendTwo = function(defend, game) {
+                    defend.retreat(0)
+                    || defend.retreat(1)
+                    || defend.retreat(2)
+                    || defend.retreat(3)
+                    || defend.retreat(4);
+                };
+
+                var verify = function(player, game) {
+                    game.end(); // Force game end to stop the game
+                    done();
+                };
+
+                game.players[0].on('turn', playTurnOne);
+                game.players[1].on('turn', playTurnTwo);
+                game.players[1].on('defend', defendTwo);
+                game.players[1].on('recover', verify);
 
                 game.start();
             });
