b880888428d168b527e1cf1b413890e683dd3489
Kenneth Lee <kenneth.lee@cbc.ca>
2014-04-07 17:18:15 -0400

    fixed a bunch of weird issues, but still having NaN for position after an attack once in awhile

--- test/game.js
+++ test/game.js
@@ -69,9 +69,20 @@ describe('Game', function() {
                 var wins = 0,
                     loses = 0;
 
-                var playTurn = function(action, game) {
+                // Prefer forward movement most of the time
+                var playTurnOne = function(action, game) {
+                    if (action.player.peice.position > 10) {
+                        moveBackwards(action) || moveForward(action);
+                    } else {
+                        moveForward(action) || moveBackward(action);
+                    }
+                };
+
+                // Prefer backward movement always
+                var playTurnTwo = function(action, game) {
                     moveBackwards(action) || moveForward(action);
                 };
+
 
                 var countWin = function() {
                     wins++;
@@ -81,8 +92,8 @@ describe('Game', function() {
                     loses++;
                 };
 
-                game.players[0].on('turn', playTurn);
-                game.players[1].on('turn', playTurn);
+                game.players[0].on('turn', playTurnOne);
+                game.players[1].on('turn', playTurnTwo);
 
                 for (var i = 0; i < 2; i++) {
                     game.players[i].on('win', countWin);
@@ -123,7 +134,17 @@ describe('Game', function() {
                 var wins = 0,
                     loses = 0;
 
-                var playTurn = function(action, game) {
+                // Prefer forward movement most of the time
+                var playTurnUp = function(action, game) {
+                    if (action.player.peice.position > 10) {
+                        moveBackwards(action) || moveForward(action);
+                    } else {
+                        moveForward(action) || moveBackwards(action);
+                    }
+                };
+
+                // Prefer backward movement always
+                var playTurnDown = function(action, game) {
                     moveBackwards(action) || moveForward(action);
                 };
 
@@ -135,10 +156,10 @@ describe('Game', function() {
                     loses++;
                 };
 
-                game.players[0].on('turn', playTurn);
-                game.players[1].on('turn', playTurn);
-                game.players[2].on('turn', playTurn);
-                game.players[3].on('turn', playTurn);
+                game.players[0].on('turn', playTurnUp);
+                game.players[1].on('turn', playTurnUp);
+                game.players[2].on('turn', playTurnDown);
+                game.players[3].on('turn', playTurnDown);
 
                 for (var i = 0; i < 4; i++) {
                     game.players[i].on('win', countWin);
@@ -231,117 +252,79 @@ describe('Game', function() {
             });
         });
 
-        it('should be able to attack and defend or die', function(done) {
+        it('should be able to attack and defend', function(done) {
             var game = new Game('1v1');
 
             game.on('init', function(err) {
 
-                var playTurnOne = function(action, game) {
-                    var two = action.getPlayer('two'),
-                        distance = two.peice.position - action.player.peice.position,
-                        attack = action.player.hand.cards.indexOf(distance);
-
-                    if (~attack) {
-                        action.attack(attack, two);    // Attack if possible
-                    } else {
-                        moveForward(action) || moveBackwards(action); // Forward!
-                    }
-                };
-
-                var playTurnTwo = function(action, game) {
-                    console.log(action.player.peice);
-                    console.log(action.player.hand);
-                    // Too close to other side
-                    if (action.player.peice.position <= 10) {
-                        return moveBackwards(action);
-                    }
-
-                    moveForward(action) || moveBackwards(action); // Forward!
+                var attackOne = function(action, game) {
+                    var two = action.getPlayer('two')
+                    action.attack(0, two);    // Attack with first card
                 };
 
                 var defendTwo = function(defend, game) {
-                    var defense = defend.player.hand.cards.indexOf(defend.attackCards[0]);
+                    defend.attackCards.should.have.property('length').and.equal(1);
+                    defend.attackCards[0].should.equal(1);
 
-                    if (~defense) {
-                        defend.block(defense);
-                        game.end();
-
-                    } else {
-                        defend.die();
-                    }
+                    defend.block(0);    // Block with first card
+                    game.end();
                 };
 
-                var verify = function() {
+                var verifyTwo = function(action, game) {
+                    action.player.hand.cards.length.should.equal(1);
+                    action.player.hand.cards[0].should.equal(2);
                     done();
                 };
 
-                game.players[0].on('turn', playTurnOne);
-                game.players[1].on('turn', playTurnTwo);
+                game.players[0].on('turn', attackOne);
                 game.players[1].on('defend', defendTwo);
+                game.players[1].on('turn', verifyTwo);
 
-                // Only one should be triggered
-                game.on('end', verify);
+                // Rig the game for the test
+                game.players[0].peice.position = 6;
+                game.players[1].peice.position = 7;
+
+                game.players[0].hand.cards = [1];    // To attack
+                game.players[1].hand.cards = [1, 2];    // To block
 
                 game.start();
             });
         });
 
-        it('should be able to dashing strike and retreat/die', function(done) {
+        it('should be able to dashing strike and retreat', function(done) {
             var game = new Game('1v1');
 
             game.on('init', function(err) {
 
-                var playTurnOne = function(action, game) {
-                    var two = action.getPlayer('two'),
-                        distance = two.peice.position - action.player.peice.position;
+                var dashingStrikeOne = function(action, game) {
+                    var two = action.getPlayer('two');
 
-                    for (var moveIndex = 0; moveIndex < 5; moveIndex++) {
-                        for (var attackIndex = moveIndex; attackIndex < 5; attackIndex++) {
-                            if (moveIndex !== attackIndex && action.player.hand.peekAt(moveIndex)[0] + action.player.hand.peekAt(attackIndex)[0] === distance) {
-                                return action.dashingStrike(moveIndex, attackIndex, two);    // Attack if possible
-                            }
-                        }
-                    }
-
-                    if (distance <= 10) {
-                        return moveBackwards(action);
-                    }
-
-                    moveForward(action) || moveBackwards(action); // Forward!
+                    action.dashingStrike(0, [1], two).should.equal(true);    // Dashing Strike!
                 };
 
-                var playTurnTwo = function(action, game) {
-                    var one = action.getPlayer('one'),
-                        distance = action.player.peice.position - one.peice.position;
+                var retreatTwo = function(defend, game) {
+                    defend.block(0).should.equal(false);    // Should not be able to block
 
-                    //console.log(action.player.peice);
-                    //console.log(action.player.hand);
-
-                    if (distance <= 5 || action.player.peice.position <= 11) {
-                        return moveBackwards(action);
-                    }
-
-                    moveForward(action) || moveBackwards(action); // Forward!
+                    defend.retreat(0);
+                    defend.player.state.should.equal(Player.states.RECOVERING);
                 };
 
-                var defendTwo = function(defend, game) {
-                    defend.retreat(0)
-                    || defend.retreat(1)
-                    || defend.retreat(2)
-                    || defend.retreat(3)
-                    || defend.retreat(4);
-                };
-
-                var verify = function(player, game) {
-                    player.state.should.equal(Player.states.RETREATING);
-                    game.end(); // Force game end to stop the game
+                var recoverTwo = function(player, game) {
+                    player.state.should.equal(Player.states.RECOVERING);
+                    game.end();
                     done();
                 };
 
-                game.players[0].on('turn', playTurnOne);
-                game.players[1].on('turn', playTurnTwo);
-                game.players[1].on('defend', defendTwo);
-                game.players[1].on('recover', verify);
+                game.players[0].on('turn', dashingStrikeOne);
+                game.players[1].on('defend', retreatTwo);
+                game.players[1].on('recover', recoverTwo);
+
+                // Rig the game for the test
+                game.players[0].peice.position = 6;
+                game.players[1].peice.position = 10;
+
+                game.players[0].hand.cards = [1, 3];    // To attack
+                game.players[1].hand.cards = [5];    // To retreat
 
                 game.start();
             });
@@ -349,7 +332,65 @@ describe('Game', function() {
 
     });
 
+    it('should be able to play a full game', function(done) {
+        var game = new Game('1v1');
 
+        game.on('init', function(err) {
+            var playTurn = function(action, game) {
+                var opponent = action.player.name === 'one' ? action.getPlayer('two') : action.getPlayer('one'),
+                    distance = Math.abs(opponent.peice.position - action.player.peice.position);
+                    attack = action.player.hand.cards.indexOf(distance);
+
+                for (var moveIndex = 0; moveIndex < 5; moveIndex++) {
+                    for (var attackIndex = moveIndex; attackIndex < 5; attackIndex++) {
+                        if (moveIndex !== attackIndex && action.player.hand.peekAt(moveIndex)[0] + action.player.hand.peekAt(attackIndex)[0] === distance) {
+                            return action.dashingStrike(moveIndex, attackIndex, opponent);    // Dashing strike if possible
+                        }
+                    }
+                }
+
+                if (~attack) {
+                    return action.attack(attack, opponent);
+                }
+
+                moveForward(action) || moveBackwards(action); // Forward!
+            };
+
+            var defend = function(defend, game) {
+                // Check if can block
+                var defense = defend.player.hand.cards.indexOf(defend.attackCards[0]);
+
+                if (~defense) {
+                    defend.block(defense);
+
+                } else {
+                    // Check if can retreat
+                    defend.retreat(0)
+                    || defend.retreat(1)
+                    || defend.retreat(2)
+                    || defend.retreat(3)
+                    || defend.retreat(4)
+                    || defend.die();
+                }
+            };
+
+            var endGame = function() {
+                done();
+            };
+
+            game.players[0].on('turn', playTurn);
+            game.players[1].on('turn', playTurn);
+
+            game.players[0].on('defend', defend);
+            game.players[1].on('defend', defend);
+
+            game.on('end', endGame);
+
+
+            game.start();
+        });
+
+    });
 
 
     it('should serialize state');
